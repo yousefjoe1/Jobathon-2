82a736acef6abca2d0a8b034442b83b97643f7b1
yousef mahmoud <yousefmahmoud150@gmail.com>
2022-06-21 11:15:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import LogIn from './components/Log_In/LogIn';
+
+
+
+function App() {
+  
+  return (
+    <>
+    <LogIn />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Log_In/LogIn.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+
+import st from "./login.module.css";
+
+const LogIn = (submit) => {
+  const [user, setUser] = useState({ user_name: "", user_password: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submit(user.user_name, user.user_password);
+  };
+
+  const handleChange = (e) => {
+    const theUser = { [e.target.name]: e.target.value };
+    setUser({ ...user, ...theUser });
+  };
+
+  return (
+    <>
+      <section className={st.login_section}>
+        <div className={st.loginDiv}>
+          <form className={st.form} onSubmit={handleSubmit}>
+            <label htmlFor="username-input">User Name</label>
+            <input
+              onChange={handleChange}
+              name="user_name"
+              id="username-input"
+              type="email"
+            />
+            <label htmlFor="password-input">User Password</label>
+            <input
+              onChange={handleChange}
+              name="user_password"
+              id="password-input"
+              type="password"
+            />
+            <br />
+            <button id="login-button" className="btn btn-primary my-2">
+              Login
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LogIn;
